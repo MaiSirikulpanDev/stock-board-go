2e8aeb84515a06fdc4cfe5211f4ec83fac65437a
Thanakorn Sirikulpan <gintonicswingjazz@gmail.com>
2026-03-28 23:48:18 +0700

    random price

--- internal/repositories/stock.repository.go
+++ internal/repositories/stock.repository.go
@@ -1,22 +1,30 @@
 package repositories
 
 import (
-	"context"
+	"math/rand"
 	"stock-board-go/internal/models"
+	"time"
 )
 
 type StockRepository interface {
-	GetStock(ctx context.Context, ticker string) (*models.Stock, error)
+	GetStock(ticker string) (*models.Stock, error)
 }
 
-type stockRepository struct {
-	stock *models.Stock
-}
+type stockRepository struct{}
 
 func NewStockRepository() StockRepository {
 	return &stockRepository{}
 }
 
-func (r *stockRepository) GetStock(ctx context.Context, ticker string) (*models.Stock, error) {
-	return r.stock, nil
+func (r *stockRepository) GetStock(ticker string) (*models.Stock, error) {
+	time.Sleep(time.Duration(rand.Intn(400)+100) * time.Millisecond)
+
+	// random price
+	price := 10.0 + rand.Float64()*490.0
+
+	return &models.Stock{
+		Ticker:     ticker,
+		Price:      price,
+		LastUpdate: time.Now(),
+	}, nil
 }
